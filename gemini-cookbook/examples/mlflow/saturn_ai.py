1bca10b90ea4acadb5b5b8666ecd340dc73d6db3
Lukasimac <lukasimac12@gmail.com>
2025-09-11 07:46:13 +0200

    chore: data

---
+++ gemini-cookbook/examples/mlflow/saturn_ai.py
@@ -0,0 +1,10 @@
+GRANITE_31_DENSE_8B = "granite3.1-dense:8b"
+model = "ibm/granite-13b-chat-v2"
+GRANITE_31_DENSE_8B_2 = "ibm/slate-30m-english-rtrvr-v2"
+GRANITE_31_DENSE_8B_3 = "ibm/granite-3-8b-instruct"
+
+def main() -> None:
+    print("Hello")
+
+if __name__ == "__main__":
+    main()
